2bb0e8248ae847fbcf6c1064c576407424e201b1
Pratuat Amatya <pratuat@gmail.com>
2026-06-30 12:54:18 +0200

    kto alignment done

--- train_kto.sh
+++ train_kto.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-export WANDB_PROJECT=FactAlign
+export WANDB_PROJECT=factalign-kto
+export WANDB_RUN_NAME=gemma-2b-v2-it_1
 export WANDB_LOG_MODEL=false
-export WANDB_DISABLED=true
+export WANDB_DISABLED=false
+export WANDB_API_KEY="..."
 export HF_HOME=${HOME}/.cache/huggingface
 export HF_TOKEN_PATH=${HOME}/.cache/huggingface/token
 
-NUM_GPUS=1
+NUM_GPUS=4
+CUDA_VISIBLE_DEVICES="1,2,6,7"
 
-ACCELERATE_LOG_LEVEL=info accelerate launch --num_processes ${NUM_GPUS} \
-    run_kto.py configs/kto_gemma_2b_zephyr_sft.yaml
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} ACCELERATE_LOG_LEVEL=info accelerate launch --num_processes ${NUM_GPUS} \
+    run_kto.py configs/kto_gemmma_2b.yaml
 
 # ACCELERATE_LOG_LEVEL=info accelerate launch --config_file configs/deepspeed_zero4.yaml --num_processes ${NUM_GPUS} \
 #     run_kto.py configs/kto_gemma_2b_zephyr_sft_deepspeed.yaml
